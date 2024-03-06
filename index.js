dbb7573dd967c85368dccd12978dc24866814552
Barahalikar Siddharth <barahalikar.siddharth@gmail.com>
2024-03-07 00:12:55 +0530

    testing core functions

--- index.js
+++ index.js
@@ -5,6 +5,46 @@ const github = require('@actions/github');
 
 async function run() {
     try {
+                                // Get an input using the getInput function
+                                const input = core.getInput('name');
+                                core.info(`Input received: ${input}`);
+
+                                // Set an output using the setOutput function
+                                core.setOutput('output_name', 'output_value');
+
+                                // Export a variable using the exportVariable function
+                                core.exportVariable('variable_name', 'variable_value');
+
+                                // Add a path using the addPath function
+                                core.addPath('/path/to/tool');
+
+                                // Register a secret using the setSecret function
+                                core.setSecret('secret_name');
+
+                                // Log messages using the info, notice, warning, and error functions
+                                core.info('Information message');
+                                core.notice('Notice message');
+                                core.warning('Warning message');
+                                core.error('Error message');
+
+                                // Check if debugging is enabled using the isDebug function
+                                if (core.isDebug()) {
+                                  core.debug('Debugging is enabled');
+                                }
+
+                                // Start a log group using the startGroup function
+                                core.startGroup('group_name');
+
+                                // Log some messages inside the group
+                                core.info('Information message inside group');
+                                core.notice('Notice message inside group');
+                                core.warning('Warning message inside group');
+                                core.error('Error message inside group');
+
+                                // End the log group using the endGroup function
+                                core.endGroup();
+
+
       const githubToken = core.getInput('github-token');
       const giphyApiKey = core.getInput('giphy-api-key');
   
